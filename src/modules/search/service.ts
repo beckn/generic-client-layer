95ad520e1cb466135299e1062e3911cfe20162ae
Ajay Nishad <ajay.nishad@eminds.ai>
2023-11-12 13:19:49 +0530

    feat: add response mapping feature

--- src/modules/search/service.ts
+++ src/modules/search/service.ts
@@ -1,5 +1,6 @@
 import { buildRequestContextVer1_1_0 } from "../../common";
 import { generateMessageForBAP } from "./schemaHelper";
+import * as mapperService from '../../common/mapper.service';
 
 export const searchService = (body: any) => {
   const context = buildRequestContextVer1_1_0(body?.context, "search");
@@ -7,6 +8,9 @@ export const searchService = (body: any) => {
   if (!message) {
     message = {};
   }
+
+
+  return mapperService.map(onSearchResponse?.responses[0]); // onSearchResponse is response from protocol server
 
   return { context, message };
 };
