988996fad06c3b20b034aec1d1e02de2def23e75
shreyvishal <shreya.vishal@eminds.ai>
2023-11-13 14:28:05 +0530

    added: search API for generic client layer

--- src/modules/search/service.ts
+++ src/modules/search/service.ts
@@ -1,16 +1,22 @@
 import { buildRequestContextVer1_1_0 } from "../../common";
-import { generateMessageForBAP } from "./schemaHelper";
-import * as mapperService from '../../common/mapper.service';
+import { generateMessageForBAP, generateResponseforCl } from "./schemaHelper";
+import * as mapperService from "../../common/mapper.service";
+import { ProtocolServer } from "../../common/protocol-server.service";
 
-export const searchService = (body: any) => {
-  const context = buildRequestContextVer1_1_0(body?.context, "search");
-  let { message } = generateMessageForBAP(body);
-  if (!message) {
-    message = {};
+export const searchService = async (body: any) => {
+  try {
+    const context = buildRequestContextVer1_1_0(body?.context, "search");
+    let { message } = generateMessageForBAP(body);
+    if (!message) {
+      message = {};
+    }
+    console.log("Payload for BAP=====> ", JSON.stringify({ context, message }));
+    const protocolServer = new ProtocolServer({ context, message });
+    const data = await protocolServer.call();
+    const responseForCl = generateResponseforCl(data.data);
+
+    return responseForCl;
+  } catch (error) {
+    console.log(error);
   }
-
-
-  return mapperService.map(onSearchResponse?.responses[0]); // onSearchResponse is response from protocol server
-
-  return { context, message };
 };
