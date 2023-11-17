44b3bc33ecb3967b07d385c09e14eeb5d546d494
Ajay Nishad <ajay.nishad@eminds.ai>
2023-11-17 19:42:29 +0530

    feat: add the remove empty object keys sanitization

--- src/common/mapper.service.ts
+++ src/common/mapper.service.ts
@@ -2,8 +2,12 @@ import fs from "fs";
 import path from "path";
 import appRootPath from 'app-root-path';
 import jsonata from 'jsonata';
+import { removeEmptyObjectKeys } from './utils'
 
-export const map = (data: any, action?: string) => {
+export const map = async (data: any, action?: string) => {
   const expression = jsonata(fs.readFileSync(path.join(appRootPath.toString(), `/mappings/${action}.jsonata`), "utf8"));
-  return expression.evaluate(data);
+
+  let mapped = await expression.evaluate(data);
+  mapped = removeEmptyObjectKeys(mapped);
+  return mapped;
 };
