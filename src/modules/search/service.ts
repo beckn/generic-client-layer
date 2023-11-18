d58f4e7c2c707aa5aa7dc143d98afcb1dfd85982
shreyvishal <shreya.vishal@eminds.ai>
2023-11-18 13:36:47 +0530

    Added: Search Request config from JSONata

--- src/modules/search/service.ts
+++ src/modules/search/service.ts
@@ -1,12 +1,16 @@
+import { buildRequestContextVer1_1_0 } from "../../common";
 import * as mapperService from "../../common/mapper.service";
 import { ProtocolServer } from "../../common/protocol-server.service";
 
 export const searchService = async (body: any) => {
   try {
-    const request = await mapperService.map(body, 'search');
-    const response = await (new ProtocolServer(request)).call();
-    return await mapperService.map(response, "on_search")
-  } catch (error) {
-    console.log(error);
+    const context = buildRequestContextVer1_1_0(body?.context, "search");
+    const request = await mapperService.map({ ...body, context }, "search");
+    console.log(JSON.stringify(request));
+    const response = await new ProtocolServer(request).call();
+
+    return await mapperService.map(response.data, "on_search");
+  } catch (error: any) {
+    console.log(error.response.data.error.data.errors);
   }
 };
