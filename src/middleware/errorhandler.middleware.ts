0fe63db5e6ee01697f157085d1769b67c72e2dd8
Abhishek Y <abhisheky@192.168.1.4>
2023-12-06 16:28:44 +0530

    Fix: Protocol server validation error handling

--- src/middleware/errorhandler.middleware.ts
+++ src/middleware/errorhandler.middleware.ts
@@ -1,12 +1,19 @@
 import { inject, injectable } from 'inversify';
 import { Request, Response, NextFunction } from 'express';
 import { AppLogger } from '../app/app.logger';
+import { AxiosError } from 'axios';
+import { TLService } from '../tl/tl.service';
 
 @injectable()
 export class ErrorHandlerMiddleware {
-    constructor(@inject(AppLogger) private logger: AppLogger) { }
+    constructor(@inject(AppLogger) private logger: AppLogger, @inject(TLService) private tlService: TLService) { }
 
-    public handleError(error: Error, req: Request, res: Response, next: NextFunction): void {
+    public async handleError(error: Error | AxiosError, req: Request, res: Response, next: NextFunction) {
+        const { response } = error as AxiosError;
+        if (response?.status === 400) {
+            const transformedResponse = await this.tlService.transform(response.data, "ps_error");
+            return res.status(response.status).json(transformedResponse);
+        }
         this.logger.error("%s", error.stack);
         const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
         res.status(statusCode).json({
