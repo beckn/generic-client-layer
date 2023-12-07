5d6f30a323b9bcbd8e06b5f48f33d3ab39959f01
Ajay Nishad <ajay.nishad@eminds.ai>
2023-12-07 20:23:31 +0530

    refactor: format code

--- src/gcl/gcl.controller.ts
+++ src/gcl/gcl.controller.ts
@@ -2,61 +2,62 @@ import { inject } from "inversify";
 import { controller, httpPost, requestBody } from "inversify-express-utils";
 import { GCLService } from "./gcl.service";
 
-@controller("/")
+@controller('/')
 export class GCLController {
-  constructor(@inject(GCLService) private service: GCLService) {}
 
-  @httpPost("search")
-  public async search(@requestBody() body: any): Promise<any> {
-    const searchResult = await this.service.search(body);
-    return searchResult;
-  }
+    constructor(@inject(GCLService) private service: GCLService) { }
 
-  @httpPost("select")
-  public async select(@requestBody() body: any): Promise<any> {
-    const selectResult = await this.service.select(body);
-    return selectResult;
-  }
+    @httpPost('search')
+    public async search(@requestBody() body: any): Promise<any> {
+        const searchResult = await this.service.search(body);
+        return searchResult;
+    }
 
-  @httpPost("init")
-  public async init(@requestBody() body: any): Promise<any> {
-    const initResult = await this.service.init(body);
-    return initResult;
-  }
+    @httpPost('select')
+    public async select(@requestBody() body: any): Promise<any> {
+        const selectResult = await this.service.select(body);
+        return selectResult;
+    }
 
-  @httpPost("confirm")
-  public async confirm(@requestBody() body: any): Promise<any> {
-    const confirmResult = await this.service.confirm(body);
-    return confirmResult;
-  }
+    @httpPost('init')
+    public async init(@requestBody() body: any): Promise<any> {
+        const initResult = await this.service.init(body);
+        return initResult;
+    }
 
-  @httpPost("status")
-  public async status(@requestBody() body: any): Promise<any> {
-    const statusResult = await this.service.status(body);
-    return statusResult;
-  }
+    @httpPost('confirm')
+    public async confirm(@requestBody() body: any): Promise<any> {
+        const confirmResult = await this.service.confirm(body);
+        return confirmResult;
+    }
 
-  @httpPost("rating")
-  public async rating(@requestBody() body: any): Promise<any> {
-    const statusResult = await this.service.rating(body);
-    return statusResult;
-  }
+    @httpPost('status')
+    public async status(@requestBody() body: any): Promise<any> {
+        const statusResult = await this.service.status(body);
+        return statusResult;
+    }
 
-  @httpPost("cancel")
-  public async cancel(@requestBody() body: any): Promise<any> {
-    const statusResult = await this.service.cancel(body);
-    return statusResult;
-  }
+    @httpPost('rating')
+    public async rating(@requestBody() body: any): Promise<any> {
+        const statusResult = await this.service.rating(body);
+        return statusResult;
+    }
 
-  @httpPost("update")
-  public async update(@requestBody() body: any): Promise<any> {
-    const updateResult = await this.service.update(body);
-    return updateResult;
-  }
+    @httpPost('cancel')
+    public async cancel(@requestBody() body: any): Promise<any> {
+        const statusResult = await this.service.cancel(body);
+        return statusResult;
+    }
 
-  @httpPost("support")
-  public async support(@requestBody() body: any): Promise<any> {
-    const updateResult = await this.service.support(body);
-    return updateResult;
-  }
+    @httpPost('update')
+    public async update(@requestBody() body: any): Promise<any> {
+        const updateResult = await this.service.update(body);
+        return updateResult;
+    }
+
+    @httpPost('support')
+    public async support(@requestBody() body: any): Promise<any> {
+        const updateResult = await this.service.support(body);
+        return updateResult;
+    }
 }
