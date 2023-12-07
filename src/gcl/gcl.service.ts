13bbc5b70081ac96d8eb28698401cde703c6492d
Ajay Nishad <ajay.nishad@eminds.ai>
2023-12-07 16:51:03 +0530

    refactor: format code

--- src/gcl/gcl.service.ts
+++ src/gcl/gcl.service.ts
@@ -4,74 +4,74 @@ import { PSClientService } from "../psclient/psclient.service";
 
 @injectable()
 export class GCLService {
-  constructor(
-    @inject(TLService) private tlService: TLService,
-    @inject(PSClientService) private psClientService: PSClientService
-  ) {}
+    constructor(
+        @inject(TLService) private tlService: TLService,
+        @inject(PSClientService) private psClientService: PSClientService
+    ) { }
 
-  async search(body: any) {
-    const payload = await this.tlService.transform(body, "search");
-    const psResponse = await this.psClientService.post(payload);
-    const response = await this.tlService.transform(psResponse, "on_search");
+    async search(body: any) {
+        const payload = await this.tlService.transform(body, "search");
+        const psResponse = await this.psClientService.post(payload);
+        const response = await this.tlService.transform(psResponse, "on_search");
 
-    return response;
-  }
+        return response;
+    }
 
-  async select(body: any) {
-    const payload = await this.tlService.transform(body, "select");
-    const psResponse = await this.psClientService.postMany(payload);
-    const response = await this.tlService.transform(psResponse, "on_select");
+    async select(body: any) {
+        const payload = await this.tlService.transform(body, "select");
+        const psResponse = await this.psClientService.postMany(payload);
+        const response = await this.tlService.transform(psResponse, "on_select");
 
-    return response;
-  }
+        return response;
+    }
 
-  async init(body: any) {
-    const payload = await this.tlService.transform(body, "init");
-    const psResponse = await this.psClientService.postMany(payload);
-    const response = await this.tlService.transform(psResponse, "on_init");
+    async init(body: any) {
+        const payload = await this.tlService.transform(body, "init");
+        const psResponse = await this.psClientService.postMany(payload);
+        const response = await this.tlService.transform(psResponse, "on_init");
 
-    return response;
-  }
+        return response;
+    }
 
-  async confirm(body: any) {
-    const payload = await this.tlService.transform(body, "confirm");
-    const psResponse = await this.psClientService.postMany(payload);
-    const response = await this.tlService.transform(psResponse, "on_confirm");
+    async confirm(body: any) {
+        const payload = await this.tlService.transform(body, "confirm");
+        const psResponse = await this.psClientService.postMany(payload);
+        const response = await this.tlService.transform(psResponse, "on_confirm");
 
-    return response;
-  }
+        return response;
+    }
 
-  async status(body: any) {
-    // const payload = await this.tlService.transform(body, "select");
-    // const psResponse = await this.psClientService.postMany(payload);
-    // const response = await this.tlService.transform(psResponse, "on_select");
+    async status(body: any) {
+        // const payload = await this.tlService.transform(body, "select");
+        // const psResponse = await this.psClientService.postMany(payload);
+        // const response = await this.tlService.transform(psResponse, "on_select");
 
-    // return response;
+        // return response;
 
-    return "In Progress";
-  }
+        return "In Progress";
+    }
 
-  async rating(body: any) {
-    const payload = await this.tlService.transform(body, "rating");
-    const psResponse = await this.psClientService.post(payload);
-    const response = await this.tlService.transform(psResponse, "on_rating");
+    async rating(body: any) {
+        const payload = await this.tlService.transform(body, "rating");
+        const psResponse = await this.psClientService.post(payload);
+        const response = await this.tlService.transform(psResponse, "on_rating");
 
-    return response;
-  }
+        return response;
+    }
 
-  async cancel(body: any) {
-    const payload = await this.tlService.transform(body, "cancel");
-    const psResponse = await this.psClientService.post(payload);
-    const response = await this.tlService.transform(psResponse, "on_cancel");
+    async cancel(body: any) {
+        const payload = await this.tlService.transform(body, "cancel");
+        const psResponse = await this.psClientService.post(payload);
+        const response = await this.tlService.transform(psResponse, "on_cancel");
 
-    return response;
-  }
+        return response;
+    }
 
-  async update(body: any) {
-    const payload = await this.tlService.transform(body, "update");
-    const psResponse = await this.psClientService.postMany(payload);
-    const response = await this.tlService.transform(psResponse, "on_update");
+    async update(body: any) {
+        const payload = await this.tlService.transform(body, "update");
+        const psResponse = await this.psClientService.postMany(payload);
+        const response = await this.tlService.transform(psResponse, "on_update");
 
-    return response;
-  }
+        return response;
+    }
 }
