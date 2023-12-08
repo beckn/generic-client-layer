d1d6a2930a37afc3a18dcae93000c80fd8da1222
Ajay Nishad <ajay.nishad@eminds.ai>
2023-12-08 15:15:17 +0530

    fix: status api issue

--- src/gcl/gcl.controller.ts
+++ src/gcl/gcl.controller.ts
@@ -4,64 +4,64 @@ import { GCLService } from "./gcl.service";
 
 @controller("/")
 export class GCLController {
-  constructor(@inject(GCLService) private service: GCLService) {}
+    constructor(@inject(GCLService) private service: GCLService) { }
 
-  @httpPost("search")
-  public async search(@requestBody() body: any): Promise<any> {
-    const searchResult = await this.service.search(body);
-    return searchResult;
-  }
+    @httpPost("search")
+    public async search(@requestBody() body: any): Promise<any> {
+        const searchResult = await this.service.search(body);
+        return searchResult;
+    }
 
-  @httpPost("select")
-  public async select(@requestBody() body: any): Promise<any> {
-    const selectResult = await this.service.select(body);
-    return selectResult;
-  }
+    @httpPost("select")
+    public async select(@requestBody() body: any): Promise<any> {
+        const selectResult = await this.service.select(body);
+        return selectResult;
+    }
 
-  @httpPost("init")
-  public async init(@requestBody() body: any): Promise<any> {
-    const initResult = await this.service.init(body);
-    return initResult;
-  }
+    @httpPost("init")
+    public async init(@requestBody() body: any): Promise<any> {
+        const initResult = await this.service.init(body);
+        return initResult;
+    }
 
-  @httpPost("confirm")
-  public async confirm(@requestBody() body: any): Promise<any> {
-    const confirmResult = await this.service.confirm(body);
-    return confirmResult;
-  }
+    @httpPost("confirm")
+    public async confirm(@requestBody() body: any): Promise<any> {
+        const confirmResult = await this.service.confirm(body);
+        return confirmResult;
+    }
 
-  @httpPost("status")
-  public async status(@requestBody() body: any): Promise<any> {
-    const statusResult = await this.service.status(body);
-    return statusResult;
-  }
+    @httpPost("status")
+    public async status(@requestBody() body: any): Promise<any> {
+        const statusResult = await this.service.status(body);
+        return statusResult;
+    }
 
-  @httpPost("rating")
-  public async rating(@requestBody() body: any): Promise<any> {
-    const statusResult = await this.service.rating(body);
-    return statusResult;
-  }
+    @httpPost("rating")
+    public async rating(@requestBody() body: any): Promise<any> {
+        const statusResult = await this.service.rating(body);
+        return statusResult;
+    }
 
-  @httpPost("cancel")
-  public async cancel(@requestBody() body: any): Promise<any> {
-    const statusResult = await this.service.cancel(body);
-    return statusResult;
-  }
+    @httpPost("cancel")
+    public async cancel(@requestBody() body: any): Promise<any> {
+        const statusResult = await this.service.cancel(body);
+        return statusResult;
+    }
 
-  @httpPost("update")
-  public async update(@requestBody() body: any): Promise<any> {
-    const updateResult = await this.service.update(body);
-    return updateResult;
-  }
+    @httpPost("update")
+    public async update(@requestBody() body: any): Promise<any> {
+        const updateResult = await this.service.update(body);
+        return updateResult;
+    }
 
-  @httpPost("support")
-  public async support(@requestBody() body: any): Promise<any> {
-    const updateResult = await this.service.support(body);
-    return updateResult;
-  }
-  @httpPost("track")
-  public async track(@requestBody() body: any): Promise<any> {
-    const trackResult = await this.service.track(body);
-    return trackResult;
-  }
+    @httpPost("support")
+    public async support(@requestBody() body: any): Promise<any> {
+        const updateResult = await this.service.support(body);
+        return updateResult;
+    }
+    @httpPost("track")
+    public async track(@requestBody() body: any): Promise<any> {
+        const trackResult = await this.service.track(body);
+        return trackResult;
+    }
 }
--- src/gcl/gcl.service.ts
+++ src/gcl/gcl.service.ts
@@ -7,7 +7,7 @@ export class GCLService {
   constructor(
     @inject(TLService) private tlService: TLService,
     @inject(PSClientService) private psClientService: PSClientService
-  ) {}
+  ) { }
 
   async search(body: any) {
     const payload = await this.tlService.transform(body, "search");
@@ -42,9 +42,9 @@ export class GCLService {
   }
 
   async status(body: any) {
-    const payload = await this.tlService.transform(body, "select");
+    const payload = await this.tlService.transform(body, "status");
     const psResponse = await this.psClientService.postMany(payload);
-    const response = await this.tlService.transform(psResponse, "on_select");
+    const response = await this.tlService.transform(psResponse, "on_status");
 
     return response;
   }
