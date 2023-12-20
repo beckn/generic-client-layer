1784697d153df62784f2134f4a5abf4a152024b9
shreyvishal <shreya.vishal@eminds.ai>
2023-12-20 10:05:25 +0530

    Enabled: Cors

--- src/App.ts
+++ src/App.ts
@@ -1,31 +1,52 @@
-import express, { Application } from 'express';
-import { container, server } from './inversify/inversify.config';
-import './gcl/gcl.controller';
-import { ConfigService } from './config/config.service';
-import { AppLogger } from './app/app.logger';
-import { ErrorHandlerMiddleware } from './middleware/errorhandler.middleware';
-
+import express, { Application, NextFunction } from "express";
+import { container, server } from "./inversify/inversify.config";
+import "./gcl/gcl.controller";
+import { ConfigService } from "./config/config.service";
+import { AppLogger } from "./app/app.logger";
+import { ErrorHandlerMiddleware } from "./middleware/errorhandler.middleware";
+import cors from "cors";
 class App {
-    public app: Application;
+  public app: Application;
 
-    constructor() {
-        this.app = express();
-        this.config();
-        this.setupMiddlewares();
-    }
+  constructor() {
+    this.app = express();
+    this.config();
+    this.setupMiddlewares();
+  }
 
-    private config(): void {
-        server.setConfig((app) => {
-            app.use(express.json());
-            app.use(express.urlencoded({ extended: true }))
-        });
-        this.app.use(server.build());
-    }
+  private config(): void {
+    server.setConfig((app) => {
+      app.use(express.json());
+      app.use(express.urlencoded({ extended: true }));
+      app.options(
+        "*",
+        cors({
+          origin: "*",
+          optionsSuccessStatus: 200,
+          credentials: true,
+          methods: ["GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS"]
+        })
+      );
 
-    private setupMiddlewares() {
-        const errorHandlerMiddleware = container.get<ErrorHandlerMiddleware>(ErrorHandlerMiddleware);
-        this.app.use(errorHandlerMiddleware.handleError.bind(errorHandlerMiddleware));
-    }
+      app.use(
+        cors({
+          origin: "*",
+          methods: ["GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS"]
+        })
+      );
+    });
+
+    this.app.use(server.build());
+  }
+
+  private setupMiddlewares() {
+    const errorHandlerMiddleware = container.get<ErrorHandlerMiddleware>(
+      ErrorHandlerMiddleware
+    );
+    this.app.use(
+      errorHandlerMiddleware.handleError.bind(errorHandlerMiddleware)
+    );
+  }
 }
 
 const configService = container.resolve<ConfigService>(ConfigService);
@@ -35,5 +56,5 @@ const app = new App().app;
 const port = configService.getAppPort();
 
 app.listen(port, () => {
-    logger.info(`Server is running on http://localhost:${port}`);
+  logger.info(`Server is running on http://localhost:${port}`);
 });
