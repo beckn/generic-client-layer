b3f4edf8b089fa65de12770f37c441b20f6eb2ec
shreyvishal <shreya.vishal@eminds.ai>
2024-01-05 18:05:28 +0530

    Added: Xinput Feature

--- src/x-input/x-input.service.ts
+++ src/x-input/x-input.service.ts
@@ -1,42 +1,82 @@
 import { inject, injectable } from "inversify";
 import { AppLogger } from "../app/app.logger";
-import { KeyValue } from '../types/common.type';
+import { KeyValue } from "../types/common.type";
 import { PSClientService } from "../psclient/psclient.service";
 import HttpClient from "../httpclient/http.service";
-
-const XINPUTKEY = 'xinput';
+import * as cheerio from "cheerio";
+const XINPUTKEY = "xinput";
 
 @injectable()
 export class XInputService {
-    constructor(
-        @inject(AppLogger) private logger: AppLogger,
-        @inject(PSClientService) private psClientService: PSClientService,
-        @inject(HttpClient) private httpClient: HttpClient
-    ) { }
+  constructor(
+    @inject(AppLogger) private logger: AppLogger,
+    @inject(PSClientService) private psClientService: PSClientService,
+    @inject(HttpClient) private httpClient: HttpClient
+  ) {}
+  async getXInputForm(data: KeyValue) {
+    // wrap this into try catch block
+    // const newData = { ...data };
+    // for (const key in data) {
+    //   //   console.log(key);
+    //   if (key === XINPUTKEY && data[key].form.mime_type === "text/html") {
+    //     const xInputRes = await this.httpClient.get<string>(data[key].form.url);
+    //     // delete data[key].form.url;
 
-    async getXInputForm(data: KeyValue) {
-        // wrap this into try catch block
-        const newData = { ...data };
-        for (const key in data) {
-            if (key === XINPUTKEY && data[key].form.mime_type === 'text/html') {
-                const xInputRes = await this.httpClient.post<string>(data[key].form.url, {
-                    context: {
-                        action: data.context.action
-                    }
-                });
-                delete data[key].form.url;
-                newData[key].form.html = this.sanatizeXInputHtml(xInputRes);
-            } else if (typeof data[key] === 'object' && data[key] !== null) {
-                this.getXInputForm(data[key]);
-            }
-        }
-        return newData;
-    }
+    //     this.someVar.message.order.items[0].xinput.form.html =
+    //       await this.sanatizeXInputHtml(xInputRes);
+    //   } else if (typeof data[key] === "object" && data[key] !== null) {
+    //     this.getXInputForm(data[key]);
+    //   }
+    // }
+    const xInputRes = await this.httpClient.get<any>(
+      data.message.order.items[0].xinput.form.url
+    );
+    delete data.message.order.items[0].xinput.form.url;
+    data.message.order.items[0].xinput.form.html = (
+      await this.sanatizeXInputHtml(xInputRes.textHtml)
+    ).html;
+    data.message.order.items[0].xinput.form.action = (
+      await this.sanatizeXInputHtml(xInputRes.textHtml)
+    ).action;
+    data.message.order.items[0].xinput.form.method = (
+      await this.sanatizeXInputHtml(xInputRes.textHtml)
+    ).method;
+    return data;
+  }
 
-    async sanatizeXInputHtml(html: string) {
-        // Get action value from form tag, value is url to which form will be submitted
-        // Encrypt action url
-        // Add hidden field in form with name as bpp_url - this of better name
-        // Remove action and method from form tag
+  async sanatizeXInputHtml(html: string) {
+    const $ = cheerio.load(html);
+    const $formAction = $("form").attr("action");
+    const $formMethod = $("form").attr("method");
+    console.log("Form Attributes", { $formAction, $formMethod });
+    $("form").attr("id", "xinputform");
+    $("form").removeAttr("action");
+    $("form").removeAttr("method");
+    $("form").append(
+      `<input type="hidden" value=${$formAction} id="action" name="action"></input>`
+    );
+    $("form").append(
+      `<input type="hidden" value=${$formMethod} id="method" name="method"></input>`
+    );
+    $("input[type='submit']").attr("id", "xinputsubmitbutton");
+    return {
+      html: $.html(),
+      action: "http://localhost:3002/submitForm",
+      method: "POST"
+    };
+  }
+
+  async submitXinputForm(body: any) {
+    try {
+      const actionUrl = body?.action;
+
+      delete body.action;
+      delete body.method;
+      const submitFormDataResp = await this.httpClient.post(actionUrl, body);
+
+      return submitFormDataResp;
+    } catch (error: any) {
+      return { message: error.message };
     }
+  }
 }
