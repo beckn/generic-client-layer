16d2685647631471d1376c8780412fa025754b33
shreyvishal <shreya.vishal@eminds.ai>
2024-01-11 19:24:18 +0530

    Fixed: Some Change

--- src/test/industry-4.0/industry-4.0.spec.ts
+++ src/test/industry-4.0/industry-4.0.spec.ts
@@ -1,59 +1,17 @@
 import "reflect-metadata";
-import { describe, it, expect, beforeEach } from "@jest/globals";
-import { Container } from "inversify";
-import { GCLController } from "../../gcl/gcl.controller";
-import { GCLService } from "../../gcl/gcl.service";
-import { XInputService } from "../../x-input/x-input.service";
-import { TLService } from "../../tl/tl.service";
-import { AppLogger } from "../../app/app.logger";
-import { PSClientService } from "../../psclient/psclient.service";
-import { ConfigService } from "../../config/config.service";
-import HttpClient from "../../httpclient/http.service";
-import { ErrorHandlerMiddleware } from "../../middleware/errorhandler.middleware";
+import { describe, it, expect, beforeEach, jest } from "@jest/globals";
 
-const container = new Container();
-// container.bind();
+import { container } from "../../inversify/inversify.config";
+import { GCLController } from "../../gcl/gcl.controller";
 
 describe("GCL Controller Testing", () => {
-  // let appLogger = new AppLogger();
-  // let configService = new ConfigService();
-  // let httpClient = new HttpClient(appLogger);
-  // let xinputService = new XInputService(appLogger, httpClient);
-  // let psClientService = new PSClientService(configService, httpClient);
-  // let tlService = new TLService(appLogger);
-  // let gclService = new GCLService(tlService, psClientService);
-  // let gclController = new GCLController(gclService, xinputService);
-  // let errorHandlerMiddleware = new ErrorHandlerMiddleware(appLogger, tlService);
+  beforeEach(async () => {});
 
-  let container: Container;
+  it("should be defined", async () => {
+    let controller = container.resolve(GCLController);
 
-  beforeEach(async () => {
-    container = new Container();
-    container.bind<GCLController>(GCLController).toSelf();
-    container.bind<GCLService>(GCLService).toSelf();
+    console.log("======>Hello");
 
-    // appLogger = new AppLogger();
-    // configService = new ConfigService();
-    // httpClient = new HttpClient(appLogger);
-    // xinputService = new XInputService(appLogger, httpClient);
-    // psClientService = new PSClientService(configService, httpClient);
-    // tlService = new TLService(appLogger);
-    // gclService = new GCLService(tlService, psClientService);
-    // gclController = new GCLController(gclService, xinputService);
-    // errorHandlerMiddleware = new ErrorHandlerMiddleware(appLogger, tlService);
-  });
-
-  it("should be defined", () => {
-    // let controller = container.resolve(GCLController);
-    let service = container.resolve(GCLService);
-
-    // const errorHandlerMiddleware = container.get<ErrorHandlerMiddleware>(
-    //     ErrorHandlerMiddleware
-    //   );
-    //   this.app.use(
-    //     errorHandlerMiddleware.handleError.bind(errorHandlerMiddleware)
-    //   );
-    console.log("======>", service);
-    expect(service).toBeDefined();
+    expect(controller).toEqual({});
   });
 });
