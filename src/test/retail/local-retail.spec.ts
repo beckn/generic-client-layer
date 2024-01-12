b85cc8dbde26e5a758b9ecae0f8c202fd49247fe
shreyvishal <shreya.vishal@eminds.ai>
2024-01-12 15:35:38 +0530

    Added: Some data

--- src/test/retail/local-retail.spec.ts
+++ src/test/retail/local-retail.spec.ts
@@ -5,11 +5,12 @@ import { container } from "../../inversify/inversify.config";
 import { GCLController } from "../../gcl/gcl.controller";
 import HttpClient from "../../httpclient/http.service";
 import SearchRequestFromUI from "./request-from-ui/search.request.json";
-import SelectRequestFromUI from "./request-from-ui/select.request.json";
-import SearcResponseFromPS from "./response-from-ps/search.response.json";
-import SelectResponseFromPS from "./response-from-ps/select.response.json";
 import SearchResponseToUI from "./response-to-ui/response.search.json";
+import SearchResponseFromPS from "./response-from-ps/search.response.json";
+
+import SelectResponseFromPS from "./response-from-ps/select.response.json";
 import SelectResponseToUI from "./response-to-ui/response.select.json";
+import SelectRequestFromUI from "./request-from-ui/select.request.json";
 
 describe("Local-retail Controller Testing", () => {
   let controller: GCLController;
@@ -21,19 +22,19 @@ describe("Local-retail Controller Testing", () => {
     expect(controller).toBeDefined();
   });
 
-  it("Search API should be working fine", async () => {
+  it.skip("Search API should be working fine", async () => {
     jest
       .spyOn(HttpClient.prototype, "post")
-      .mockImplementation(async () => SearcResponseFromPS);
+      .mockImplementation(async () => SearchResponseFromPS);
     const data = await controller.search(SearchRequestFromUI);
     expect(JSON.stringify(data)).toEqual(JSON.stringify(SearchResponseToUI));
   });
 
-  it.only("Select API should be working fine", async () => {
+  it("Select API should be working fine", async () => {
     jest
       .spyOn(HttpClient.prototype, "post")
       .mockImplementation(async () => SelectResponseFromPS);
-    const data = await controller.search(SelectRequestFromUI);
+    const data = await controller.select(SelectRequestFromUI);
     expect(JSON.stringify(data)).toEqual(JSON.stringify(SelectResponseToUI));
   });
 });
