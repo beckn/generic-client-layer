0f844f41bbee386aefd01f490f8f761934428a4e
ShivaniDhoke <shivani.dhoke@eminds.ai>
2024-01-12 14:55:25 +0530

    added UT for select api

--- src/test/retail/local-retail.spec.ts
+++ src/test/retail/local-retail.spec.ts
@@ -5,8 +5,12 @@ import { container } from "../../inversify/inversify.config";
 import { GCLController } from "../../gcl/gcl.controller";
 import HttpClient from "../../httpclient/http.service";
 import SearchRequestFromUI from "./request-from-ui/search.request.json";
+import SelectRequestFromUI from "./request-from-ui/select.request.json";
 import SearcResponseFromPS from "./response-from-ps/search.response.json";
+import SelectResponseFromPS from "./response-from-ps/select.response.json";
 import SearchResponseToUI from "./response-to-ui/response.search.json";
+import SelectResponseToUI from "./response-to-ui/response.select.json";
+
 describe("Local-retail Controller Testing", () => {
   let controller: GCLController;
   beforeAll(async () => {
@@ -23,5 +27,13 @@ describe("Local-retail Controller Testing", () => {
       .mockImplementation(async () => SearcResponseFromPS);
     const data = await controller.search(SearchRequestFromUI);
     expect(JSON.stringify(data)).toEqual(JSON.stringify(SearchResponseToUI));
+  });
+
+  it.only("Select API should be working fine", async () => {
+    jest
+      .spyOn(HttpClient.prototype, "post")
+      .mockImplementation(async () => SelectResponseFromPS);
+    const data = await controller.search(SelectRequestFromUI);
+    expect(JSON.stringify(data)).toEqual(JSON.stringify(SelectResponseToUI));
   });
 });
